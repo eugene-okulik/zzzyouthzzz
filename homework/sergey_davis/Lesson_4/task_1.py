3bcf893742b25de8fe40f09b3d0ea890e0d2ad98
Sergey Davis <zzzyouthzzz@gmail.com>
2026-05-14 00:36:30 +0400

    Homework_4 done

---
+++ homework/sergey_davis/Lesson_4/task_1.py
@@ -0,0 +1,28 @@
+my_dict = {
+    'tuple': (1, 2, 3, 4, 5),
+    'list': ['apple', 'orange', 'banana', 'grape', 'kiwi'],
+    'dict': {
+        'name': 'Sergey',
+        'age': 36,
+        'city': 'Ulyanovsk',
+        'course': 'Python',
+        'level': 'beginner'
+    },
+    'set': {10, 20, 30, 40, 50}
+}
+
+print(my_dict['tuple'][-1])
+
+my_dict['list'].append('avocado')
+my_dict['list'].pop(1)
+
+my_dict['dict']['i am a tuple'] = 'some value'
+my_dict['dict'].pop('age')
+
+my_dict['set'].add(60)
+my_dict['set'].remove(20)
+
+print(my_dict)
+
+
+
