eef32e20e7adfadf03cfabbd402dda79178d6973
Sergey Davis <zzzyouthzzz@gmail.com>
2026-05-15 21:18:14 +0400

    fixed tuple key in dictionary

--- homework/sergey_davis/Lesson_4/task_1.py
+++ homework/sergey_davis/Lesson_4/task_1.py
@@ -16,7 +16,7 @@ print(my_dict['tuple'][-1])
 my_dict['list'].append('avocado')
 my_dict['list'].pop(1)
 
-my_dict['dict']['i am a tuple'] = 'some value'
+my_dict['dict'][('i am a tuple',)] = 'some value'
 my_dict['dict'].pop('age')
 
 my_dict['set'].add(60)
